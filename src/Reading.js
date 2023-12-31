425159f7982604b26a21d14759ebe97990a4362e
21020793 <100114904+21020793@users.noreply.github.com>
2023-12-31 16:44:43 +0700

    fix minor bugs

--- src/Reading.js
+++ src/Reading.js
@@ -13,7 +13,7 @@ const Reading = ({ API_URL }) => {
     const [chapterList, setChapterList] = useState([]);
     const [loading, setLoading] = useState(true);
     const [isOpen, setIsOpen] = useState(false);
-
+    const CurrentChapter = parseInt(chapterIDs, 10);
     useEffect(() => {
         const fetchItems = async () => {
             try {
@@ -184,7 +184,7 @@ const Reading = ({ API_URL }) => {
     //         </div>
     //     </div>
     // )
-
+    
     return (
         <div>
             {!fetchError && !loading &&
@@ -193,7 +193,7 @@ const Reading = ({ API_URL }) => {
                         <div className='Previous'><Link to={`/book/${title}/${chapterIDs - 1}`}><FontAwesomeIcon icon={faArrowLeft} className='faLeft_arrow' /></Link></div>
                         <div className='Return_Home'><Link to={'/'}><FontAwesomeIcon icon={faHouse} className='HouseIcon' /></Link></div>
                         <div className='List_Info'><button onClick={handleClick}><FontAwesomeIcon icon={faList} className='List_Icon' /></button></div>
-                        <div className='NextB'><Link to={`/book/${title}/${chapterIDs + 1}`}><FontAwesomeIcon icon={faArrowRight} className='faRight_arrow' /></Link></div>
+                        <div className='NextB'><Link to={`/book/${title}/${CurrentChapter + 1}`}><FontAwesomeIcon icon={faArrowRight} className='faRight_arrow' /></Link></div>
                     </section>
                     {isOpen && (
                         <section id='RChapter_list'>
